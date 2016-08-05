cdf0427deafa87f7a1f298ac0b2bf29bf301a386
David Nguyen <hungphuongjc@gmail.com>
2016-08-05 11:22:36 +0800

    update confirm/cancel choice list

--- CFOP/Speech/CommonSpeechChoices.cs
+++ CFOP/Speech/CommonSpeechChoices.cs
@@ -2,7 +2,7 @@ namespace CFOP.Speech
 {
     public class CommonSpeechChoices
     {
-        public static string[] ConfirmChoices() => new[] { "OK", "Confirm", "Yes", "Yes Please", "Sure" };
-        public static string[] CancelChoices() => new[] { "No", "Cancel", "Nevermind", "Cancel" };
+        public static string[] ConfirmChoices() => new[] { "OK", "Confirm", "Yes", "Yes Please", "Sure", "Go Ahead" };
+        public static string[] CancelChoices() => new[] { "No", "Cancel", "Nevermind" };
     }
 }
